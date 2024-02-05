f4472373d5966b2bb8f746294f8be6abc1d18f15
Aagam Shah <aagam.jtdev@gmail.com>
2024-02-05 11:35:07 +0530

    Footer is now stick to the bootom to the page

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -4,16 +4,16 @@ import Text from '../text/Text';
 
 function Footer() {
   return (
-    <footer className={classes.footer}>
-      <Text>
-        Find the perfect
-        content
-        for you
-      </Text>
-      <Text className={classes.copyright}>
-        © “My-content” 2024 All right reserved.
-      </Text>
-    </footer>
+    <div className={classes.container}>
+      <footer className={classes.footer}>
+        <Text>
+          Find the perfect content for you
+        </Text>
+        <Text className={classes.copyright}>
+          © “My-content” 2024 All right reserved.
+        </Text>
+      </footer>
+    </div>
   );
 }
 
