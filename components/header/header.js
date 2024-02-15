2d80f5ca87ad8f51c2f9b3cb1ebd36af20ff4fa2
Aagam Shah <aagam.jtdev@gmail.com>
2024-02-15 10:32:52 +0530

    Ui Fixes - Making colors same all over the site

--- components/header/header.js
+++ components/header/header.js
@@ -4,11 +4,12 @@ import { useRouter } from 'next/router';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import classes from './header.module.scss';
 
 function Header() {
   const router = useRouter();
 
-  const isActive = (path) => (router.pathname === path ? { color: '#E85D04' } : { color: '#FFFFFF' });
+  const isActive = (path) => (router.pathname === path ? { color: '#c45f20' } : { color: '#FFFFFF' });
 
   const elementStyle = {
     '--bs-bg-opacity': 0,
@@ -25,6 +26,7 @@ function Header() {
       <Container>
         <Link href="/" passHref>
           <Navbar.Brand
+            className={classes.linkText}
             style={{
               font: 'caption',
               fontSize: 'xx-large',
@@ -45,18 +47,25 @@ function Header() {
           <Nav className="me-auto">
             <Link href="/savedBooks" passHref>
               <Nav.Link
+                className={classes.linkText}
                 style={{ ...isActive('/savedBooks'), userSelect: 'none' }}
               >
                 Saved Books
               </Nav.Link>
             </Link>
             <Link href="/addBook" passHref>
-              <Nav.Link style={{ ...isActive('/addBook'), userSelect: 'none' }}>
+              <Nav.Link
+                className={classes.linkText}
+                style={{ ...isActive('/addBook'), userSelect: 'none' }}
+              >
                 Add Books
               </Nav.Link>
             </Link>
             <Link href="/books" passHref>
-              <Nav.Link style={{ ...isActive('/books'), userSelect: 'none' }}>
+              <Nav.Link
+                className={classes.linkText}
+                style={{ ...isActive('/books'), userSelect: 'none' }}
+              >
                 Explore Books
               </Nav.Link>
             </Link>
